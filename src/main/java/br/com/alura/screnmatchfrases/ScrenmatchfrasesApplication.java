b98e717eefd65c8285ff12aa7706b50b9794f29c
jvictornascimento <victojoao789@gmail.com>
2024-07-01 21:55:35 -0300

    inited challenge alura third course

---
+++ src/main/java/br/com/alura/screnmatchfrases/ScrenmatchfrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screnmatchfrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrenmatchfrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrenmatchfrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/screnmatchfrases/ScrenmatchfrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screnmatchfrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrenmatchfrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
