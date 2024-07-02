471585fc1724019921cb013bdba18d73c994c4f4
jvictornascimento <victojoao789@gmail.com>
2024-07-01 23:09:47 -0300

    configurations service in controller layer

--- src/main/java/br/com/alura/screnmatchfrases/controller/FraseController.java
+++ src/main/java/br/com/alura/screnmatchfrases/controller/FraseController.java
@@ -1,5 +1,8 @@
 package br.com.alura.screnmatchfrases.controller;
 
+import br.com.alura.screnmatchfrases.model.dto.FraseDTO;
+import br.com.alura.screnmatchfrases.services.FraseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("series")
 public class FraseController {
+    @Autowired
+    private FraseService service;
 
     @GetMapping("/frases")
-    public String frases(){
-        return "chagamos aqui!";
+    public FraseDTO frases(){
+        return service.getOneFraseRandon();
     }
 
 
